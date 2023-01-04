4cea48d6690f3d392f5bcfbb863d6c6a1efa613b
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2023-01-03 21:48:48 -0500

    4th commit // app finished

--- src/pages/services-plans/services.jsx
+++ src/pages/services-plans/services.jsx
@@ -1,11 +1,47 @@
-import './services.css';
-import React from 'react';
-import Data from '../../data';
+import "./services.css";
+import React from "react";
+import Data from "../../data";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import Card from "../../UI/Card";
+import { plans } from "../../data";
 
 const services = () => {
   return (
-    <div>services</div>
-  )
-}
+    <>
+      <Header title="Our Services" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nisi,
+        asperiores praesentium quam sint ratione magni quod enim facere
+        distinctio consequuntur fugit repudiandae maxime odit ad placeat ex eos
+        voluptatem?
+      </Header>
+      <section className="plans">
+        <div className="container plans_container">
+          {
+            plans.map(({id,name,description,price,features})=>{
+              return(
+                <Card key={id} className='plan'>
+                  <h3>{name}</h3>
+                  <small>{description}</small>
+                  <h1>{`$${price}`}</h1><h2>/mo</h2>
+                  <h4>Features</h4>
+                  {
+                    features.map(({feature, available}, index)=>{
+                      return (
+                        <p key={index} className={!available ? 'disabled' : ''}>{feature}</p>
+                      )
+                    })
+                  }
+                  <button className="btn lg">Choose Plan</button>
+                </Card>
+                
+              )
+            })
+          }
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default services
+export default services;
--- src/pages/trainers/trainers.jsx
+++ src/pages/trainers/trainers.jsx
@@ -1,10 +1,42 @@
-import './trainers.css'; 
-import React from 'react'
+import "./trainers.css";
+import React from "react";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_5.jpg";
+import { trainers } from "../../data";
+import { BsInstagram } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import Trainer from "../../components/Trainer";
 
-const trainers = () => {
+const Trainers = () => {
   return (
-    <div>trainers</div>
-  )
-}
+    <>
+      <Header title="Our Trainers" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo amet
+        rerum assumenda perferendis mollitia ipsum alias nemo velit, eum, vel
+        voluptatum sequi accusantium laborum dolor! Soluta quasi sunt voluptatem
+        explicabo?
+      </Header>
+      <section className="trainers">
+        <div className="container trainers_container">
+          {trainers.map(({ id, image, name, job, socials }) => {
+            return (
+              <Trainer key={id} image={image} name={name} job={job}
+                socials={
+                  [
+                    {icon: <BsInstagram></BsInstagram>, link: socials[0]},
+                    {icon: <AiOutlineTwitter></AiOutlineTwitter>, link: socials[1]},
+                    {icon: <FaFacebookF></FaFacebookF>, link: socials[2]},
+                    {icon: <FaLinkedinIn></FaLinkedinIn>, link: socials[3]},
+                  ]
+                }
+              ></Trainer>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default trainers
+export default Trainers;
